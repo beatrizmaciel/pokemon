b14d8db795f57b64aebc55a1a1ea86ecd73daa3a
beatrizmaciel <beatrizcbmaciel@gmail.com>
2021-10-11 17:15:38 -0300

    mudanças de fonte

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ export function Home() {
     return (
         <main className="main-home">
             <h1>Quem é esse</h1>
-            <h1>Pokemón?!</h1>
+            <h1>POKÉMON?!</h1>
             <button>Começar</button>
         </main>
     )
